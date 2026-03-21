9d5052672b10943a15e782f1d4facd6a2c141c8b
Better Call Sarah <sarahalencar6409@gmail.com>
2026-03-21 11:12:55 -0300

    Melhorias em entitular

--- main.js
+++ main.js
@@ -2,35 +2,42 @@ const input = require('prompt-sync')();
 const figlet = require('figlet');
 
 // Função para criar um cabeçalho com descrição e opções para navegação
-function entitular(cabeçalho, descrição, opções) {
+function entitular(cabeçalho, descrição='', opções=[]) {
     while (true) {
         // Cabeçalho
         console.clear();
         console.log(figlet.textSync(cabeçalho));
 
         // Descrição
-        const descriçãoDividida = descrição.split('\n');
-        for (linha in descriçãoDividida) {
-            console.log(`  ${descriçãoDividida[linha]}`);
+        if (descrição) {
+            const descriçãoDividida = descrição.split('\n');
+            for (linha in descriçãoDividida) {
+                console.log(`  ${descriçãoDividida[linha]}`);
+            }
+            console.log()
         }
-        console.log()
 
         // Opções
-        let i = 1
-        for (opção in opções) {
-            console.log(`${i} -> ${opções[opção]}`)
-            i++
-        }
+        if (opções.length > 0) {
+            let i = 1
+            for (opção in opções) {
+                console.log(`${i} -> ${opções[opção]}`)
+                i++
+            }
 
-        // Entrada do Usuário
-        let entrada = input('>> ');
-        let entradaInt = parseInt(entrada)
+            // Entrada do Usuário
+            let entrada = input('>> ');
+            let entradaInt = parseInt(entrada)
 
-        // Validação de Entrada
-        if (entrada == 'sair') {
-            process.exit();
-        } else if (entradaInt >= 1 && entradaInt <= opções.length) {
-            return entradaInt
+            // Validação de Entrada
+            if (entrada == 'sair' || entrada == 'quit') {
+                process.exit();
+            } else if (entradaInt >= 1 && entradaInt <= opções.length) {
+                return entradaInt
+            }
+
+        } else {
+            break
         }
     }
 }
@@ -38,11 +45,12 @@ function entitular(cabeçalho, descrição, opções) {
 
 // Main
 function main() {
-    entitular(
-        'No Light Below',
-        'Bem-vindo ao melhor\nJogo em terminal que você já viu!',
-        ['Jogar', 'Configurações', 'Ajuda', 'Créditos', 'Sair']
-    );
+  entitular('sla')
+  //entitular(
+  //    'No Light Below',
+  //    'Bem-vindo ao melhor\nJogo em terminal que você já viu!',
+  //    ['Jogar', 'Configurações', 'Ajuda', 'Créditos', 'Sair']
+  //);
 }
 
 main()
